d1c4c95c81659acf0a9c85588be1f1e555d0ff06
Weslei França <weslwifranca@gmail.com>
2023-10-30 15:46:52 -0300

    update

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,7 +10,7 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/inscreva-se" element={<Register />} />
       <Route path="/home" element={<Home />} />
 
       <Route path="/" element={<PrivateRoutes />}>
